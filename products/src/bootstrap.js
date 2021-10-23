25bbf095adb07f904b631a13623726546fb226c6
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-23 20:25:59 +1100

    Refactoring products

--- products/src/bootstrap.js
+++ products/src/bootstrap.js
@@ -1,13 +1,15 @@
 import faker from 'faker';
 
-let products = '';
+const mount = (el) => {
+  let products = '';
 
-for (let i = 0; i < 3; i++) {
-  const name = faker.commerce.productName();
-  products += `<div>${name}</div>`;
-}
+  for (let i = 0; i < 3; i++) {
+    const name = faker.commerce.productName();
+    products += `<div>${name}</div>`;
+  }
 
-document.querySelector('#dev-products').innerHTML = products;
+  el.innerHTML = products;
+};
 
 // Context/Situation #1
 // We are running this file in development in isolation
@@ -15,8 +17,21 @@ document.querySelector('#dev-products').innerHTML = products;
 // Which DEFINITELY has an element with an id of 'dev-products'
 // We want to immediately render our app into that element
 
+// @ts-ignore
+if (process.env.NODE_ENV === 'development') {
+  const el = document.querySelector('#dev-products');
+  // Assuming our container doesnt have an element
+  // with id 'dev-products'...
+  if (el) {
+    // We are probably running in isolation
+    mount(el);
+  }
+}
+
 // Context/Situation #2
 // We are running this file file in development or production
 // through the CONTAINER app
 // NO GUARANTEE that an element with an id of 'dev-products'
 // WE DO NOT WANT try to immediately render the app
+
+export { mount };
