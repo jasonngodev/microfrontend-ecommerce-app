89761375d061c289c5085e79b620733b3700a0fb
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-23 20:44:28 +1100

    Refactoring Cart

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,7 +1,9 @@
 import 'cart/CartShow';
 
-import { mount } from 'products/ProductsIndex';
+import { mount as cartMount } from 'cart/CartShow';
+import { mount as productsMount } from 'products/ProductsIndex';
 
 console.log('Container');
 
-mount(document.querySelector('#my-products'));
+productsMount(document.querySelector('#my-products'));
+cartMount(document.querySelector('#my-cart'));
