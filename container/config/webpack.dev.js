c3bf8f4f4e22bd5b1f5d94650364d9b50db9aed8
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-26 23:02:08 +1100

    Integration of container and marketing

--- container/config/webpack.dev.js
+++ container/config/webpack.dev.js
@@ -1,7 +1,6 @@
 const { merge } = require('webpack-merge');
-
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
 const commonConfig = require('./webpack.common');
 
 const devConfig = {
@@ -13,6 +12,13 @@ const devConfig = {
     },
   },
   plugins: [
+    new ModuleFederationPlugin({
+      name: 'container',
+      remotes: {
+        marketing: 'marketing@http://localhost:8081/remoteEntry.js',
+      },
+    }),
+
     new HtmlWebpackPlugin({
       template: './public/index.html',
     }),
--- marketing/src/App.js
+++ marketing/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Landing from './components/Landing';
 import Pricing from './components/Pricing';
 import React from 'react';
-import { StylesProvider } from '@material-ui/styles';
+import { StylesProvider } from '@material-ui/core/styles';
 
 export default () => {
   return (
