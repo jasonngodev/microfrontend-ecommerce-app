ae7d18fc391c5bff2484ba8a8e0f7f2e1940b5d8
Kiet Pham <kietpham.dev@gmail.com>
2021-10-27 23:10:27 +1100

    generic integration

--- container/src/App.js
+++ container/src/App.js
@@ -1,9 +1,12 @@
+import MarketingApp from './components/MarketingApp';
 import React from 'react';
-// @ts-ignore
-import { mount } from 'marketing/MarketingApp';
-
-console.log(mount);
 
 export default () => {
-  return <h1>Hi there!</h1>;
+  return (
+    <div>
+      <h1>Hi there!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  );
 };
