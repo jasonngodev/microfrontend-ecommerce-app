b46875a31a48a84b8b7009519b6bbc7b512cbf1a
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-30 17:54:57 +1100

    randomize production classnames

--- container/src/App.js
+++ container/src/App.js
@@ -1,15 +1,26 @@
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from '@material-ui/core/styles';
+
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 import React from 'react';
 
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'co',
+});
+
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header signedIn={undefined} onSignOut={undefined} />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header signedIn={undefined} onSignOut={undefined} />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
