4b7329338dd3c32f1111fe0bf5854d86a0f2161a
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-30 16:48:53 +1100

    Added header

--- container/src/App.js
+++ container/src/App.js
@@ -1,13 +1,15 @@
+import { BrowserRouter } from 'react-router-dom';
+import Header from './components/Header';
 import MarketingApp from './components/MarketingApp';
 import React from 'react';
 
 export default () => {
   return (
-    <div>
-      <h1>Hi there, I'm container!</h1>
-      <h2>Working on branch container-dev</h2>
-      <hr />
-      <MarketingApp />
-    </div>
+    <BrowserRouter>
+      <div>
+        <Header signedIn={undefined} onSignOut={undefined} />
+        <MarketingApp />
+      </div>
+    </BrowserRouter>
   );
 };
