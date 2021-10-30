8307bb2d60b80654b8929d1b4a6a0518c27063a1
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-30 13:33:24 +1100

    updated text

--- container/src/App.js
+++ container/src/App.js
@@ -4,7 +4,7 @@ import React from 'react';
 export default () => {
   return (
     <div>
-      <h1>Hi there, I'm container</h1>
+      <h1>Hi there, I'm container!!</h1>
       <hr />
       <MarketingApp />
     </div>
