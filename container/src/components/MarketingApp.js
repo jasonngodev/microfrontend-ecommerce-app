6fdb3da1c81799376000248c20b3307893adcbec
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-31 23:23:04 +1100

    Syncing history object

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -2,12 +2,21 @@ import React, { useEffect, useRef } from 'react';
 
 // @ts-ignore
 import { mount } from 'marketing/MarketingApp';
+import { useHistory } from 'react-router-dom';
 
 export default () => {
   const ref = useRef(null);
+  const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current);
+    mount(ref.current, {
+      onNavigate: ({ pathname: nextPathname }) => {
+        const { pathname } = history.location;
+        if (pathname !== nextPathname) {
+          history.push(nextPathname);
+        }
+      },
+    });
   });
 
   return <div ref={ref} />;
