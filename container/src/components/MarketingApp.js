0d7114151eea77097e305267ce8f45fcf1362acc
Kiet Pham <kiet@octopusgroup.com.au>
2021-10-31 23:35:45 +1100

    container to child communication

--- container/src/components/MarketingApp.js
+++ container/src/components/MarketingApp.js
@@ -9,7 +9,7 @@ export default () => {
   const history = useHistory();
 
   useEffect(() => {
-    mount(ref.current, {
+    const { onParentNavigate } = mount(ref.current, {
       onNavigate: ({ pathname: nextPathname }) => {
         const { pathname } = history.location;
         if (pathname !== nextPathname) {
@@ -17,7 +17,9 @@ export default () => {
         }
       },
     });
-  });
+
+    history.listen(onParentNavigate);
+  }, []);
 
   return <div ref={ref} />;
 };
